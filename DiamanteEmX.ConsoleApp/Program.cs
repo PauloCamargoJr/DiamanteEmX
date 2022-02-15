e26b01039b446621a42b736007a02d179c828646
Paulo Camargo <paulo13765@outlook.com>
2022-02-15 15:52:57 -0300

    Redução de linhas

--- DiamanteEmX.ConsoleApp/Program.cs
+++ DiamanteEmX.ConsoleApp/Program.cs
@@ -9,87 +9,57 @@ namespace DiamanteEmX.ConsoleApp
             Console.WriteLine("Insira um número ímpar:");
             int x = Convert.ToInt32(Console.ReadLine());
             int l = x;
-            
             if(x%2 == 1)
             {
                 Console.ForegroundColor = ConsoleColor.Cyan;
                 for (int i = 0; i < x; i++)
                 {
-
                     if(i%2 == 1)
                     {
-
                         for(int k = l; k > 0;)
                         {
-
                             Console.Write(" ");
                             k -= 2;
                         }
-
                     } 
-
                     for (int j = 0; j < i; j++)
                     {
-
                         if (i % 2 == 1)
                         {
-
                             Console.Write("X");
-
                         }
-
                     }
-
                     if (i % 2 == 1)
                         Console.Write("\n");
-
                     l--;
-
                 }
-
                 for (int i = x; i > 0; i--)
                 {
-
                     if (i % 2 == 1)
                     {
-
                         for (int k = 0; k < l;)
                         {
-
                             Console.Write(" ");
                             k += 2;
                         }
-
                     }
-
                     for (int j = 0; j < i; j++)
                     {
-
                         if (i % 2 == 1)
                         {
-
                             Console.Write("X");
-
                         }
-
                     }
-
                     if (i % 2 == 1)
                         Console.Write("\n");
-
                     l++;
-
                 }
-
             }
             else
             {
-
                 Console.ForegroundColor = ConsoleColor.Red;
                 Console.WriteLine("Valor par não pode ser aceito!");
-
             }
-
             Console.ReadKey();
         }
     }
